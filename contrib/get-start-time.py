50a0e75cae8b3e9217c4820ada3e48caca20d717
Desuwa <desuwa@desuwa.net>
2021-06-18 18:11:26 -0700

    Fix get-start-time, again

--- contrib/get-start-time.py
+++ contrib/get-start-time.py
@@ -1,29 +1,11 @@
-#! /usr/bin/env python
+#! /bin/sh
 # Call with the video URL or ID.
 # Useful for videos missing chat replays or unarchived streams, but it needs to
 # be used while the stream is live.
 # Doesn't really help if the stream had chunks removed due to editing.
 
-import urllib
-import re
-import sys
-import requests
-import json
-import datetime
 
-try:
-    vid = re.search(r"(v=|be/|^)([a-zA-Z0-9_-]{11})", sys.argv[1]).group(2)
-except:
-    print("Could not identify video ID")
-    sys.exit(1)
+id=$(echo "$1" | grep -oE "(v=|be/|^)([a-zA-Z0-9_-]{11})" | sed -E 's/.*(\/|=)//')
+ts=$(curl "https://www.youtube.com/watch?v=$id" | grep -oE '"startTimestamp":"[^"]+"' | sed -E 's/.*"([^"]+)"/\1/')
+python -c "import datetime; import sys; print(int(datetime.datetime.fromisoformat(sys.argv[1]).timestamp()))" "$ts"
 
-resp = requests.get(
-    f"https://www.youtube.com/get_video_info?video_id={vid}&html5=1"
-).content.decode("utf-8")
-
-jsn = json.loads(urllib.parse.parse_qs(resp)["player_response"][0])
-
-startTime = jsn["microformat"]["playerMicroformatRenderer"][
-    "liveBroadcastDetails"]["startTimestamp"]
-
-print(int(datetime.datetime.fromisoformat(startTime).timestamp()))
