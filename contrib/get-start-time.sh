e490ac103aeb2d708f3720e43b325905c9545f17
Desuwa <desuwa@desuwa.net>
2024-02-17 20:16:32 -0800

    Parse RFC3339 strings as start_time, works with recent ytarchive update

--- contrib/get-start-time.sh
+++ contrib/get-start-time.sh
@@ -2,10 +2,10 @@
 # Call with the video URL or ID.
 # Useful for videos missing chat replays or unarchived streams, but it needs to
 # be used while the stream is live.
-# Doesn't really help if the stream had chunks removed due to editing.
+# Isn't perfect if chunks were removed or the stream went offline temporarily.
 
 
 id=$(echo "$1" | grep -oE "(v=|be/|^)([a-zA-Z0-9_-]{11})" | sed -E 's/.*(\/|=)//')
 ts=$(curl "https://www.youtube.com/watch?v=$id" | grep -oE '"startTimestamp":"[^"]+"' | sed -E 's/.*"([^"]+)"/\1/')
-python -c "import datetime; import sys; print(int(datetime.datetime.fromisoformat(sys.argv[1]).timestamp()))" "$ts"
+echo "$ts"
 
